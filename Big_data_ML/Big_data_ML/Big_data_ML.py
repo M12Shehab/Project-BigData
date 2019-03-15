7234a89aab1a8155f082fb7f5a8715a7a9e788b6
Mohammed Ali Shehab <mohammed_shihab@daad-alumni.de>
2019-03-15 02:58:24 -0400

    improve RF model

--- Big_data_ML/Big_data_ML/Big_data_ML.py
+++ Big_data_ML/Big_data_ML/Big_data_ML.py
@@ -4,8 +4,11 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import RFECV
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler, normalize
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 from sklearn.externals import joblib
@@ -14,6 +17,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import isnan, when, count, col
 
 from data_types import *
+
+from knn_impute import knn_impute
 
 
 def read_csv(filePath):
@@ -163,6 +168,23 @@ def exrport_model(model, filename = 'finalized_model.sav'):
     # save the model to disk
     joblib.dump(model, filename)
 
+def time_ser_ML():
+    #Time searsie
+    import statsmodels.api as sm
+
+    logreg = sm.Logit(X_train, y_train)
+    logreg = logreg.fit(disp=0)
+    X_train['Prob'] = logr.predict(X_train[cols])
+    print("Train report...")
+    print(classification_report(y_train, X_train['Prob'] ))
+    #print(report)
+    print('Exporting model ....')
+    exrport_model(logreg,'logreg.sav')
+    
+    print('Predicting data ....')
+    y_pred = rfe.predict(X_test)
+
+
 def train_model(Sample_size, features, label, ALL_DATA = False):
     if ALL_DATA:
         train = pd.read_csv("./train.csv", 
@@ -173,32 +195,63 @@ def train_model(Sample_size, features, label, ALL_DATA = False):
                             dtype= col_to_load,
                             usecols= col_to_load.keys(),
                             nrows= Sample_size)
-    
+    #vv = ['AVProductStatesIdentifier','AVProductsInstalled','OsBuild',
+    #   'CountryIdentifier','Processor','SmartScreen','Census_OSVersion',
+    #   'Census_ProcessorCoreCount','Census_ProcessorModelIdentifier', 
+    #   'Census_HasOpticalDiskDrive','Census_TotalPhysicalRAM',
+    #   'Census_InternalPrimaryDisplayResolutionVertical','Census_OSInstallTypeName',
+    #   'Census_PowerPlatformRoleName','Census_IsTouchEnabled',
+    #   'Wdft_IsGamer']
+    #features =vv
     le = LabelEncoder()
     # Convert categorty type to int
     for cols in train:
         if train[cols].dtype.name == 'category':
             train[cols] = le.fit_transform(train[cols].astype(str))
         free_memory()
-    train = train.fillna(0)
+    train = train.fillna(train.mean())
     train = reduce_mem_usage(train)
     free_memory()
     X_train, X_test, y_train, y_test = train_test_split(train[features], train[label], test_size=0.3, random_state=2018)
+
+    ## Standarize features
+    scaler = StandardScaler(with_mean=True)
+    X_std = scaler.fit_transform(X_train)
+    Y_std = scaler.fit_transform(X_test)
     
-    logreg = LogisticRegression(C=0.1 ,penalty='l1',n_jobs = -1, random_state= 2018)
-    #results  = cross_val_score(logreg, X_train, y_train, scoring='roc_auc', cv=3, n_jobs = -1)  
-    logreg.fit(X_train, y_train)
+
+    logreg = LogisticRegression(C=0.1 ,penalty='l1', random_state= 2018, solver='liblinear',max_iter=100,verbose=True)
+    rfe = RFECV(estimator=logreg, cv=4, scoring='roc_auc',n_jobs = -1)
+
+    ##results  = cross_val_score(logreg, X_train, y_train, scoring='roc_auc', cv=3, n_jobs = -1)  
+    print('Training model ....')
+    X_new = rfe.fit_transform(X_std, y_train)
+    
+    #features_index = rfe.get_support(indices=True)
+    
+    #print(report)
+    print('Exporting model ....')
     exrport_model(logreg,'logreg.sav')
-    y_pred = logreg.predict(X_test)
+    
+    print('Predicting data ....')
+    y_pred = rfe.predict(X_test)
+
+    Y_new = rfe.transform(Y_std)
+    rfe.fit(X_new,y_train)
+    y_pred = rfe.predict(Y_new)
     #y_pred = cross_val_predict(logreg, X_test, y_test, scoring='roc_auc', cv=3, n_jobs = -1)
     #print('\ntest results = {}\n'.format(y_pred))
     #print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(np.mean(results)))
-
+    print("Test report...")
     confusionmatrix = confusion_matrix(y_test, y_pred)
     print(confusionmatrix)
+    #beta = np.sort(logreg.coef_)
+    #plt.plot(beta)
+    #plt.ylabel('Beta Coefficients')
+    #plt.show()
 
     print(classification_report(y_test, y_pred))
-    proba = logreg.predict_log_proba(X_test)
+    proba = rfe.predict_log_proba(Y_new)
     #proba = cross_val_predict(logreg, X_test, y_test, scoring='roc_auc', cv=3, method='predict_proba', n_jobs = -1)
     logit_roc_auc = roc_auc_score(y_test, y_pred)
     fpr, tpr, thresholds = roc_curve(y_test, proba[:,1])
@@ -211,8 +264,188 @@ def train_model(Sample_size, features, label, ALL_DATA = False):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver operating characteristic')
     plt.legend(loc="lower right")
-    plt.savefig('Log_ROC')
+    plt.savefig('Log_ROCofLogReg')
     plt.show()
+
+
+    # Plot number of features vs CV scores
+    plt.figure()
+    plt.xlabel('k')
+    plt.ylabel('CV accuracy')
+    plt.plot(np.arange(1, rfe.grid_scores_.size+1), rfe.grid_scores_)
+    plt.show()
+
+def train_modelRF(Sample_size, features, label, ALL_DATA = False):
+    if ALL_DATA:
+        train = pd.read_csv("./train.csv", 
+                            dtype= col_to_load,
+                            usecols= col_to_load.keys())
+    else:
+        train = pd.read_csv("./train.csv", 
+                            dtype= col_to_load,
+                            usecols= col_to_load.keys(),
+                            nrows= Sample_size)
+    le = LabelEncoder()
+    # Convert categorty type to int
+    for cols in train:
+        if train[cols].dtype.name == 'category':
+            train[cols] = le.fit_transform(train[cols].astype(str))
+        free_memory()
+    train = train.fillna(train.mean())
+    train = reduce_mem_usage(train)
+    free_memory()
+    
+    X_train, X_test, y_train, y_test = train_test_split(train[features], train[label], test_size=0.3, random_state=2018)
+
+    ## Standardizing the features
+    #X_train = StandardScaler(with_std=True).fit_transform(X_train)
+    #X_test = StandardScaler(with_std=True).fit_transform(X_test)
+
+    #pca = PCA(n_components=9,random_state=2018)
+    #X_train = pca.fit_transform(X_train)
+    #X_test = pca.fit_transform(X_test)
+
+    ## Standardizing the features
+    #X_train = StandardScaler(with_std=True).fit_transform(X_train)
+    #X_test = StandardScaler(with_std=True).fit_transform(X_test)
+
+    X_train = normalize(X_train, norm='l2')
+    X_test = normalize(X_test, norm='l2')
+
+    cv = 3
+    rf = RandomForestClassifier(random_state=2018, n_jobs = -1, verbose=True,criterion='entropy',max_depth=25,min_samples_split=10,n_estimators=1001)
+    rfe = RFECV(estimator=rf, cv=4, scoring='roc_auc',n_jobs = -1)
+    #score  = cross_val_score(rf, X_train, y_train, scoring='roc_auc', cv=cv, n_jobs = -1)  
+    #print("Training score = {}".format(str(score.mean())))
+
+
+    #y_pred = cross_val_predict(rf, X_test, y_test, cv=cv)
+    ##print("Testing score = {}".format(str(prediction_cv.mean())))
+    #print("Test report...")
+    #confusionmatrix = confusion_matrix(y_test, y_pred)
+    #print(confusionmatrix)
+    ##beta = np.sort(logreg.coef_)
+    ##plt.plot(beta)
+    ##plt.ylabel('Beta Coefficients')
+    ##plt.show()
+
+    #print(classification_report(y_test, y_pred))
+    ##proba =cross_val_predict(rf, X_test, y_test, cv=cv,method='predict_proba')
+    ##proba = cross_val_predict(logreg, X_test, y_test, scoring='roc_auc', cv=3, method='predict_proba', n_jobs = -1)
+    ##logit_roc_auc = roc_auc_score(y_test, y_pred)
+    ##fpr, tpr, thresholds = roc_curve(y_test, proba[:,1])
+    ##plt.figure()
+    ##plt.plot(fpr, tpr, label='Random Forest (area = %0.2f)' % logit_roc_auc)
+    ##plt.plot([0, 1], [0, 1],'r--')
+    ##plt.xlim([0.0, 1.0])
+    ##plt.ylim([0.0, 1.05])
+    ##plt.xlabel('False Positive Rate')
+    ##plt.ylabel('True Positive Rate')
+    ##plt.title('Receiver operating characteristic')
+    ##plt.legend(loc="lower right")
+    ##plt.savefig('RandomForest_ROC')
+    #plt.show()
+
+    print('Training model ....')
+    X_new = rfe.fit_transform(X_train, y_train)
+    
+    #features_index = rfe.get_support(indices=True)
+    
+    #print(report)
+    print('Exporting model ....')
+    exrport_model(logreg,'logreg.sav')
+    
+    print('Predicting data ....')
+    y_pred = rfe.predict(X_test)
+
+    X_test = rfe.transform(X_test)
+    rfe.fit(X_train,y_train)
+    y_pred = rfe.predict(X_test)
+   
+    print("Test report...")
+    confusionmatrix = confusion_matrix(y_test, y_pred)
+    print(confusionmatrix)
+   
+
+    print(classification_report(y_test, y_pred))
+    proba = rfe.predict_log_proba(X_test)
+
+    logit_roc_auc = roc_auc_score(y_test, y_pred)
+    fpr, tpr, thresholds = roc_curve(y_test, proba[:,1])
+    plt.figure()
+    plt.plot(fpr, tpr, label='Random Forest (area = %0.2f)' % logit_roc_auc)
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig('RandomForest_ROC')
+    plt.show()
+
+
+def fill_missing_data_Knn(Sample_size = 1000):
+
+    all_features = list(col_to_load.keys())[:68]
+    cols_with_missing_to_process
+    labels = [r for r in cols_with_missing_to_process if r  in all_features]
+    features = [r for r in all_features if r not in cols_with_missing_to_process]
+
+    train = pd.read_csv("./train.csv", 
+                        dtype= col_to_load,
+                        usecols= col_to_load.keys(),
+                        nrows= Sample_size
+                        )
+    #df_missing = train[labels].copy()
+    #print("Missing data frame\n")
+    print(train.info())
+    #print("\n-----------------------\nNon missing data\n")
+    
+    #df = train[features].copy()
+    #print(df.info())
+    df_final = pd.DataFrame()
+    cols_error =[]
+    for label in labels:
+        print("Processing "+label)
+        if train[label].dtype.name == 'category':
+            print("WARINING category type detected !!!")
+            try:
+                df_n  = knn_impute(target= train[label],\
+                   attributes= train[features],\
+                   k_neighbors=13,\
+                   aggregation_method='mode',\
+                  numeric_distance='euclidean',\
+                  categorical_distance='hamming', missing_neighbors_threshold=0.8)
+            except:
+                cols_error.append(label)
+                print("Error with column "+label)
+                continue
+        else:
+            df_n  = knn_impute(target= train[label],\
+               attributes= train[features],\
+               k_neighbors=13,\
+               aggregation_method='mean',\
+              numeric_distance='euclidean',\
+              categorical_distance='hamming', missing_neighbors_threshold=0.8)
+        
+        df = pd.DataFrame(data=df_n,columns=[label])
+        #df_final = df_final.join(df_n)
+        df_final = pd.concat([df_final, df_n], axis=1)
+        free_memory()
+        print("\n+++++++++++++++++++++++\n")
+        #print(df_final.info())
+        #print("\n+++++++++++++++++++++++\n")
+        #count_nan = len(df_n) - df_n.count()
+        #print("before {}\nafter {}".format(count_nan_brofe,count_nan))
+    #df_final = df_final.drop(cols_error,axis=1)
+    df_final = pd.concat([df_final, train[features]], axis=1)
+    print(df_final.info())
+    del train
+    free_memory()
+    df_final.to_csv('new_train.csv',index= None,header=True)
+    return 0
+
 
 def test_pararmeter(Sample_size, features, label):
     from sklearn.model_selection import GridSearchCV
@@ -229,21 +462,38 @@ def test_pararmeter(Sample_size, features, label):
             train[cols] = le.fit_transform(train[cols].astype(str))
             free_memory()
     train = train.fillna(0)
-
-    train = reduce_mem_usage(train)
+    train = reduce_mem_usage(train)   
     free_memory()
+
     X_train, X_test, y_train, y_test = train_test_split(train[features], train[label], test_size=0.3, random_state=2018)
+    
+    ## Standarize features
+    scaler = StandardScaler(with_mean=True)
+    X_std = scaler.fit_transform(X_train)
+    Y_std = scaler.fit_transform(X_test)
+
     # set dictionary of para
-    grid={"C":np.logspace(-3,3,7), "penalty":["l1","l2"]}# l1 lasso l2 ridge
+    grid={"n_estimators":[1001,3001], 
+          "criterion":["gini","entropy"],
+          "max_depth":[17,25,71],
+          "min_samples_split":[2, 5, 10]}# l1 lasso l2 ridge
 
     #Start testing
     print('Start test ...')
-    logreg=LogisticRegression()
-    logreg_cv=GridSearchCV(logreg, grid, cv=5, scoring='roc_auc', n_jobs = -1)
-    logreg_cv.fit(X_train,y_train)
+    #logreg=LogisticRegression()
+    #logreg_cv=GridSearchCV(logreg, grid, cv=5, scoring='roc_auc', n_jobs = -1)
+    #logreg_cv.fit(X_train,y_train)
+    #print('\n------------------------\nEnd test\n')
+    #print("tuned hpyerparameters :(best parameters) ",logreg_cv.best_params_)
+    #print("accuracy :",logreg_cv.best_score_)
+
+
+    rf = RandomForestClassifier(random_state=2018,n_jobs = -1, verbose=True)
+    rf_cv = GridSearchCV(rf, grid, cv=5, scoring='roc_auc', n_jobs = -1)
+    rf_cv.fit(X_train,y_train)
     print('\n------------------------\nEnd test\n')
-    print("tuned hpyerparameters :(best parameters) ",logreg_cv.best_params_)
-    print("accuracy :",logreg_cv.best_score_)
+    print("tuned hpyerparameters :(best parameters) ",rf_cv.best_params_)
+    print("accuracy :",rf_cv.best_score_)
 
 
 def main():
@@ -251,9 +501,10 @@ def main():
    ks = list(col_to_load)
    features = ks[:68]
    label ='HasDetections'
-   #test_pararmeter(1000000, features,label)
-   train_model(5000,features,label)
-   
+   #test_pararmeter(100000, features,label)
+   train_modelRF(10000, features,label)
+   #train_model(10000,features,label)
+   #fill_missing_data_Knn(Sample_size=10000)
 
 
 if __name__ =='__main__':
